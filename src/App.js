4b75d7401acbd9365a9675e919972e37f54872b7
Raphael Zenou <42899564+raphaelzenou@users.noreply.github.com>
2022-08-15 13:10:12 +0100

    feat: added styled component in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
+import styled, { css } from "styled-components";
 import logo from "./logo.svg";
-import "./App.css";
 import { AppContext } from "./AppContext";
 
 function App() {
@@ -14,27 +14,76 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container className="App">
+      <Header className="App-header">
+        <Logo src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <Link
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Link>
 
-        <button className="App-button" onClick={handleClick}>
+        <Button primary className="App-button" onClick={handleClick}>
           {`Clicks: ${appData.clicks}`}
-        </button>
-      </header>
-    </div>
+        </Button>
+      </Header>
+    </Container>
   );
 }
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 1em 1em;
+  padding: 0.25em 1em;
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+const Container = styled.div`
+  text-align: center;
+`;
 
 export default App;
